4ac595dcbb731de8971287f2a1059f2bc5523117
ThalesLJ <thaleslimadejesus@gmail.com>
2020-10-05 23:30:05 -0300

    Getting data from MongoDB and set to Jobs-List page with basic layout, and testing the routes

--- workut/src/app/jobs-list/jobs-list.page.ts
+++ workut/src/app/jobs-list/jobs-list.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-jobs-list',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobsListPage implements OnInit {
 
-  constructor() { }
+  constructor(public http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.searchJobs();
+  }
+
+  body = {
+    "company": {
+      "email": "workut@uam.com",
+      "password": "UAM123"
+    }
+  }
+
+  Jobs: any;
+
+  searchJobs() {
+    this.http.post('https://webhooks.mongodb-realm.com/api/client/v2.0/app/workut-nbyci/service/API/incoming_webhook/CompanyJobsSearch', this.body)
+      .subscribe(
+        (response) => {
+          this.Jobs = response;
+        },
+        (error) => {
+          console.log('Error: ' + error);
+        }
+      );
+  }
+
+  editJob(job) {
+    console.log(job);
   }
 
 }
