75d0910616b5f55aa8ac0e78ced61e7a26da796e
Mateus Donaire <canalmateusdonaire@gmail.com>
2020-10-06 22:41:40 -0300

    List data from MongoDB

--- workut/src/app/user-profile/user-profile.page.ts
+++ workut/src/app/user-profile/user-profile.page.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+
 
 @Component({
-  selector: 'app-user-profile',
-  templateUrl: './user-profile.page.html',
-  styleUrls: ['./user-profile.page.scss'],
+  selector: "app-user-profile",
+  templateUrl: "./user-profile.page.html",
+  styleUrls: ["./user-profile.page.scss"],
 })
 export class UserProfilePage implements OnInit {
-
-  constructor() { }
+  constructor(public httpClient: HttpClient) {}
 
   ngOnInit() {
+    this.search();
   }
 
+  user = {
+    email: "aa",
+    password: "a",
+  };
+
+  data: any;
+  email: string;
+  name: string;
+  lastname: string;
+
+  search() {
+    this.httpClient
+      .post(
+        "https://webhooks.mongodb-realm.com/api/client/v2.0/app/workut-nbyci/service/API/incoming_webhook/UserListProfile",
+        this.user
+      )
+      .subscribe(
+        (response) => {
+          if(response == 404){
+            console.log('Sessão expirada');
+          }else{
+            this.data = response;
+            this.email = this.data.email;
+            this.name = this.data.name;
+            this.lastname = this.data.lastname;
+          }
+          
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
 }
