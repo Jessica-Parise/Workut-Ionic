af01cb9a0286535f2f7f7c4070ca39e457e81966
Jessica-Parise <jparise.contato@hotmail.com>
2020-10-11 19:35:10 -0300

    Creating job listing page with mongoDB data

--- workut/src/app/home/home.page.ts
+++ workut/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  Jobs;
+  body = {
+    "email": "jessica@workut.com",
+    "password": "senha"
+  }
+  
+  constructor(public http: HttpClient, private router: Router){}
 
+  ngOnInit(){
+    this.searchJobs();
+  }
+
+  searchJobs(){
+    this.http.post('https://webhooks.mongodb-realm.com/api/client/v2.0/app/workut-nbyci/service/API/incoming_webhook/listJobs', this.body)
+      .subscribe(
+        (response) => {
+          this.Jobs = response;
+        },
+        (error) => {
+          console.log('Error: ' + error);
+        }
+      );
+  }
 }
