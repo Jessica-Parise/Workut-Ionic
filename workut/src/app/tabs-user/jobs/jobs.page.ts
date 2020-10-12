507ac861d5a957625f6a45d7c11046837f562cd3
Jessica-Parise <jparise.contato@hotmail.com>
2020-10-12 00:01:32 -0300

    Creating job listings with mongoDB data and validating logout

--- workut/src/app/tabs-user/jobs/jobs.page.ts
+++ workut/src/app/tabs-user/jobs/jobs.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-jobs',
@@ -8,10 +10,37 @@ import { Router } from '@angular/router';
 })
 export class JobsPage implements OnInit {
 
-  constructor(private router: Router) { }
+  Jobs;
+  body = {
+    "email": "jessica@workut.com",
+    "password": "senha"
+  }
+  constructor(public http: HttpClient, public alertController: AlertController, private router: Router) { }
 
   ngOnInit() {
-    
+    this.searchJobs();
+  }
+
+  searchJobs(){
+    this.http.post('https://webhooks.mongodb-realm.com/api/client/v2.0/app/workut-nbyci/service/API/incoming_webhook/listJobs', this.body)
+      .subscribe(
+        (response) => {
+          this.Jobs = response;
+        },
+        (error) => {
+          this.statusAlert('Erro', 'An error occurred. Please try again!');
+        }
+      );
+  }
+
+  async statusAlert(title, message) {
+    const alert = await this.alertController.create({
+      header: title,
+      message: message,
+      buttons: ['OK']
+    });
+
+    await alert.present();
   }
 
   Logout() {
