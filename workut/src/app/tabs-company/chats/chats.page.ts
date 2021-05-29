c0fd644c70143c30fd7c9bd599c1cf9a3de8fa2b
ThalesLJ <thaleslimadejesus@gmail.com>
2021-05-29 13:55:41 -0300

    Fix Chat System

--- workut/src/app/tabs-company/chats/chats.page.ts
+++ workut/src/app/tabs-company/chats/chats.page.ts
@@ -146,9 +146,9 @@ export class ChatsPage implements OnInit {
       let owner = true;
       for (let i = 0; i < response.length; i++) {
         if (response[i].contactType === '2') {
-          owner = true;
-        } else {
           owner = false;
+        } else {
+          owner = true;
         }
         this.addItemWithText(response[i].message, owner);
         this.qteMsg++;
@@ -165,9 +165,9 @@ export class ChatsPage implements OnInit {
       let owner = true;
       for (let i = this.qteMsg; i < response.length; i++) {
         if (response[i].contactType === '2') {
-          owner = true;
-        } else {
           owner = false;
+        } else {
+          owner = true;
         }
         this.addItemWithText(response[i].message, owner);
         this.qteMsg++;
